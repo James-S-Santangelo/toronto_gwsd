5946dce15c368593d028e176db330321383c2cd4
James Santangelo <james.santangelo37@gmail.com>
2021-06-25 22:44:19 -0400

    Initial commit of file to transfer raw reads

---
+++ workflow/scripts/shell/sync_raw_reads_to_graham.sh
@@ -0,0 +1,4 @@
+rsync -vuar -P \
+    --dry-run \
+    /node1nfs/archive/data/trifolium/toronto_gwsd/128.120.88.251/X202SC20030702-Z01-F001/raw_data/ \
+    santang3@gra-dtn1.computecanada.ca:/home/santang3/scratch/toronto_gwsd/data/raw/raw_reads/
