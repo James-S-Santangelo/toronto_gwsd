8bff945a90b17f9a757e91fdfbe71560553e2bf5
James Santangelo <james.santangelo37@gmail.com>
2021-07-19 16:43:03 -0400

    Change to syncing script

--- workflow/scripts/shell/sync_graham_to_niagara.sh
+++ workflow/scripts/shell/sync_graham_to_niagara.sh
@@ -1,7 +1,3 @@
 rsync -vuar -P \
-    --prune-empty-dirs \
-    --include '*/' \
-    --include 'program_resources/**' \
-    --exclude '*' \
-    ../../../results/ \
-    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/toronto_gwsd/results
+    ../../../results/program_resources/ \
+    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/toronto_gwsd/results/program_resources
