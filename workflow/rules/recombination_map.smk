b68c52f264682ce6c53e2e19bfb3e2a169e171bb
James Santangelo <james.santangelo37@gmail.com>
2023-03-24 18:36:42 -0400

    Updating recombination map interpolation to use new linkage map

--- workflow/rules/recombination_map.smk
+++ workflow/rules/recombination_map.smk
@@ -1,29 +1,6 @@
 # Rules for fitting and interpolating recombination maps
 # Relies on genetic positions from Olsen et al. 2021 and GBS marker sequences sent by Olsen
 
-rule blast_markers:
-    input:
-        markers = ancient('{0}/{{map_pop}}_tags.fa'.format(GENMAP_RESOURCE_DIR)),
-        db_flag = rules.makeblastdb_fromRef.output,
-        ref = REFERENCE_GENOME 
-    output:
-        '{0}/{{map_pop}}_marker_blast.txt'.format(GENMAP_RESULTS_DIR)
-    conda: '../envs/recombination_map.yaml'
-    log: LOG_DIR + '/blast_markers/{map_pop}_marker_blast.log'
-    params: 
-        outfmt = "'6 qseqid sseqid pident length mismatch gapopen qstart qend qlen sstart send slen evalue bitscore qcovs qcovhsp'"
-    shell:
-        """
-        blastn -db {input.ref} \
-            -query {input.markers} \
-            -out {output} \
-            -outfmt {params.outfmt} \
-            -num_threads {threads} \
-            -evalue 1e-10 \
-            -max_hsps 5 \
-            -max_target_seqs 5 2> {log}  
-        """
-
 rule sites_toInterpolate_byChrom:
     input:
         lambda w: expand(rules.remove_duplicate_sites.output, chrom=w.chrom, site_type='snps', miss='0')
@@ -36,20 +13,14 @@ rule sites_toInterpolate_byChrom:
 
 rule interpolate_genetic_map:
     input:
-        DG_markers = expand(rules.blast_markers.output, map_pop='DG'),
-        SG_markers = expand(rules.blast_markers.output, map_pop='SG'),
-        DG_genMap = ancient('{0}/DG_genMap.csv'.format(GENMAP_RESOURCE_DIR)),
-        SG_genMap = ancient('{0}/SG_genMap.csv'.format(GENMAP_RESOURCE_DIR)),
-        DG_names = ancient('{0}/DG_marker_key.csv'.format(GENMAP_RESOURCE_DIR)),
-        SG_names = ancient('{0}/SG_marker_key.csv'.format(GENMAP_RESOURCE_DIR)),
+        linkage_map = config['genmap'],
         sites = expand(rules.sites_toInterpolate_byChrom.output, chrom=CHROMOSOMES)
     output:
-        markers_byPop_plot = '{0}/genMap/markers_bothPops_withOutliers_allChroms.pdf'.format(FIGURES_DIR),
-        scamFits_plot = '{0}/genMap/scamFits_allChroms.pdf'.format(FIGURES_DIR),
+        scamFits_plot = '{0}/genMap/scamFits_allChroms.pdf'.format(GENMAP_RESULTS_DIR),
         genMap_interp = '{0}/genMap_interpolated_allChroms.txt'.format(GENMAP_RESULTS_DIR)
     conda: '../envs/recombination_map.yaml'
     notebook:
-        "../notebooks/genMap_interpolation.r.ipynb"
+        "../scripts/r/snakemake/genMap_interpolation.r"
 
 rule split_genMap:
     input:
@@ -63,7 +34,6 @@ rule split_genMap:
 
 rule recombination_map_done:
     input:
-        expand(rules.blast_markers.output, map_pop = ['SG', 'DG']),
         expand(rules.sites_toInterpolate_byChrom.output, chrom = CHROMOSOMES),
         expand(rules.split_genMap.output, chrom = CHROMOSOMES)
     output:
--- workflow/scripts/r/snakemake/genMap_interpolation.R
+++ workflow/scripts/r/snakemake/genMap_interpolation.R
@@ -8,149 +8,52 @@
 # Load requird packaes
 library(tidyverse)
 library(patchwork)
-library(slider)
 library(scam)
+
+# Load sites files for interpolation
+sites_allChroms <- snakemake@input[['sites']] %>%
+  purrr::map_dfr(., read_delim, delim='\t', col_names=c('chrom', 'pos'))
+
+# Load file with linkage map for all chromosomes
+allMaps <- read_csv(snakemake@input[['linkage_map']])
+
+# Creat list with chromosome names
+chroms <- c('Chr01_Occ', 'Chr01_Pall', 'Chr02_Occ', 'Chr02_Pall', 'Chr03_Occ', 
+            'Chr03_Pall', 'Chr04_Occ', 'Chr04_Pall', 'Chr05_Occ', 'Chr05_Pall', 
+            'Chr06_Occ', 'Chr06_Pall', 'Chr07_Occ', 'Chr07_Pall', 'Chr08_Occ', 
+            'Chr08_Pall')
 
 # Load functions used throughout script
 snakemake@source('./functions.R')
 
-# Assign input files to variables
-DG_markers <- load_marker_blast(snakemake@input[['DG_markers']]) %>%
-    mutate(pop = 'DG')
-SG_markers <- load_marker_blast(snakemake@input[['SG_markers']]) %>%
-    mutate(pop = 'SG')
-
-# Load both genetic maps from Olsen et al. 
-DG_genMap <- load_genMap(snakemake@input[['DG_genMap']]) %>%
-    mutate(pop = 'DG')
-SG_genMap <- load_genMap(snakemake@input[['SG_genMap']]) %>%
-    mutate(pop = 'SG')
-
-# Load files with marker names
-DG_names <- read.csv(snakemake@input[['DG_names']], col.names = c('qseqid', 'name')) %>%
-    mutate(pop = 'DG')
-SG_names <- read.csv(snakemake@input[['SG_names']], col.names = c('qseqid', 'name')) %>%
-    mutate(pop = 'SG')
-
-# Load sites files for interpolation
-sites_allChroms <- snakemake@input[['sites']] %>%
-    purrr::map_dfr(., read_delim, delim='\t', col_names=c('chrom', 'pos'))
-
-#################################################
-#### FILTERING MARKERS AND REMOVING OUTLIERS ####
-#################################################
-
-# Combine markers from both mapping populations
-all_markers <- bind_rows(SG_markers, DG_markers)
-
-# Combine genetic maps from both populations
-all_maps <- bind_rows(SG_genMap, DG_genMap)
-
-# Combine marker names
-all_names <- bind_rows(SG_names, DG_names)
-
-# Find markers with only a single 100% match to genome
-markers_onlyOneExact <- all_markers %>%
-	group_by(prev_id, pop) %>%
-	mutate(is_exact_match = ifelse(pident == 100, 1, 0)) %>%
-	summarise(n = n(),
-			n_exact = sum(is_exact_match)) %>%
-	filter(n_exact == 1)
-
-# Filter markers to only include those with a single match 
-markers_blast_exact <- all_markers %>%
-	filter(pident == 100) %>%
-	filter(prev_id %in% markers_onlyOneExact$prev_id) %>%
-	dplyr::select(qseqid, prev_chrom, prev_pos, prev_id, curr_chrom, curr_pos, curr_id, pop) %>%
-	left_join(., all_names, by = c('qseqid', 'pop')) %>%
-	left_join(., all_maps, by = c('name', 'pop'))
- 
-# Remove markers where Linkage group from crosses doesn't match chromosome from BLASTing to genome
-# Identify outlier markers using marker-based, sliding window, plus distance to nearest markers
-z_cut <- 1  # Z-score cutoff for outliers
-num_markers <- 6  # Number of markers to left and right of focal marker in window
-marker_dist_cut <- 1e7  # Maximum distance to nearest markers to be included
-markers_blast_exact_filt <- markers_blast_exact %>%
-	filter(LG == curr_chrom) %>%
-	dplyr::select(curr_chrom, curr_pos, pop, cM) %>%
-	rename("chrom" = "curr_chrom",
-		 "pos" = "curr_pos") %>%
-	group_by(chrom, pop) %>%
-	arrange(pos, .by_group = TRUE) %>%
-
-	# Points with no markers within `marker_dist_cut` will be excluded
-	mutate(prev_pos = lag(pos),
-		 next_pos = lead(pos),
-		 prev_pos_dist = pos - prev_pos,
-		 next_pos_dist = next_pos - pos,
-		 lone_marker = case_when(is.na(prev_pos_dist) & next_pos_dist > marker_dist_cut ~ 1,
-								 is.na(next_pos_dist) & prev_pos_dist > marker_dist_cut ~ 1,
-								 prev_pos_dist > marker_dist_cut & next_pos_dist > marker_dist_cut ~ 1,
-								 TRUE ~ 0)) %>%
-
-	# Exclude markers above or below `z_cut` z-scores in windows considering `num_markers` to left and right
-	mutate(win_mean = slide_dbl(cM, mean, .before = num_markers, .after = num_markers),
-		 win_sd = slide_dbl(cM, sd, .before = num_markers, .after = num_markers),
-		 marker_z = (cM - win_mean) / win_sd,
-		 exclude = case_when(marker_z > z_cut | marker_z < -z_cut ~ 1,
-							 lone_marker == 1 ~ 1,
-							 TRUE ~ 0)) %>%
-
-	# Remove DG map for chromosome 11 since missing markers for > 50% of chromosome
-	filter(!(chrom == 'CM019111.1' & pop == 'DG')) %>%
-
-	dplyr::select(chrom, pos, pop, cM, exclude)
-
-# Remove outliers
-markers_blast_exact_filt_noOut <- markers_blast_exact_filt %>%
-	filter(exclude != 1)
-
-# Plot markers, colored by mapping population, with outliers marked as X. All chromosomes
-chroms <- markers_blast_exact_filt %>% pull(chrom) %>% unique()
-markerPlot_byPop_list <- chroms %>%
-	purrr::map(., plot_markers_byPop, df = markers_blast_exact_filt)
-
-# Combine an save plots
-markers_allChroms_byPop <- wrap_plots(markerPlot_byPop_list, 4, 4)
-ggsave(filename = snakemake@output[['markers_byPop_plot']], plot = markers_allChroms_byPop, 
-	   device = 'pdf', dpi = 600, width = 20, height = 20, units = 'in')
-
-#########################################
-#### SMOOTH MAPS AND FIT SCAM MODELS ####
-#########################################
-
-# Get mean cM across both maps in windows
-winMeans_df <- chroms %>%
-    purrr::map_dfr(., calculate_windowed_means, 
-                   window_size = 5000000,
-                   step = 2500000,
-                   df = markers_blast_exact_filt_noOut)
+#########################
+#### FIT SCAM MODELS ####
+#########################
 
 # Fit SCAM model separately for each chromosome
 scamFits_allChroms <- chroms %>%
-    purrr::map_dfr(., get_fits, markers_df = winMeans_df, sites_df = sites_allChroms) 
+  purrr::map_dfr(., get_fits, markers_df = allMaps, sites_df = sites_allChroms)
 
 # Find minimum predicted cM
 min_cM <- scamFits_allChroms %>% 
-	group_by(chrom) %>% 
-	summarise(min_cM = min(preds))
+  group_by(chrom) %>% 
+  summarise(min_cM = min(preds))
 
 # Force minimum predicted cM to 0 for SCAM fits
-scamFits_allChroms <- scamFits_allChroms %>% 
-	group_by(chrom) %>% 
-	mutate(preds = case_when(min(preds) < 0 ~ preds + abs(min(preds)),
-							 min(preds) > 0 ~ preds - abs(min(preds))))
+scamFits_allChroms_off <- scamFits_allChroms %>% 
+  group_by(chrom) %>% 
+  mutate(preds = case_when(min(preds) < 0 ~ preds + abs(min(preds)),
+                           min(preds) > 0 ~ preds - abs(min(preds))))
 
-# Offset markers by same amount as fits. Only for plotting
-winMeans_df <- winMeans_df %>% 
-	left_join(., min_cM, by = 'chrom') %>% 
-	group_by(chrom) %>% 
-	mutate(win_mean_cM_off = case_when(min_cM < 0 ~ win_mean_cM + abs(min_cM),
-									   min_cM > 0 ~ win_mean_cM - abs(min_cM)))
+allMaps_offset <- allMaps %>% 
+  left_join(., min_cM, by = 'chrom') %>%
+  group_by(chrom) %>%
+  mutate(cM = case_when(min_cM < 0 ~ cM + abs(min_cM),
+                        min_cM > 0 ~ cM - abs(min_cM)))
 
 # List of plots. Each plot is fit of SCAM model to chromosome markers
 scamFits_plotList <- chroms %>%
-    purrr::map(., scam_plot, markers_df = winMeans_df, scam_fits_df = scamFits_allChroms)
+  purrr::map(., scam_plot, markers_df = allMaps_offset, scam_fits_df = scamFits_allChroms_off)
 
 # Combine list of plots into one
 scamFits_allChroms_plot <- wrap_plots(scamFits_plotList, 4, 4)
