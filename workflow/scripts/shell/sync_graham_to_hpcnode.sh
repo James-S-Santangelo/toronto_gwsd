61771ed4364b9da7eceaf1682f7c8b4af49cccf6
James Santangelo <james.santangelo37@gmail.com>
2023-05-24 01:21:06 -0400

    Reference genome path fix

--- workflow/scripts/shell/sync_graham_to_hpcnode.sh
+++ workflow/scripts/shell/sync_graham_to_hpcnode.sh
@@ -1,4 +1,4 @@
 rsync -vuar -P \
     --dry-run \
-    santang3@gra-dtn1.computecanada.ca:/home/santang3/scratch/gwsd/\
-    /scratch/projects/trifolium/gwsd/
+    /scratch/projects/trifolium/gwsd/results/bam/ \
+    santang3@gra-dtn1.computecanada.ca:/home/santang3/scratch/gwsd/results/bam/
