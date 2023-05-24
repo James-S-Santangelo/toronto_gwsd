f1ccdb99cc75a8c5d1f0bdbc0c575442a5aa2cd6
James Santangelo <james.santangelo37@gmail.com>
2023-05-23 21:12:44 -0400

    Changing to new GFF3

--- workflow/scripts/shell/sync_graham_to_hpcnode.sh
+++ workflow/scripts/shell/sync_graham_to_hpcnode.sh
@@ -1,11 +1,3 @@
-# rsync -vuar -P --delete \
-    # santang3@gra-dtn1.computecanada.ca:/home/santang3/scratch/gwsd/logs/ \
-    # /scratch/projects/trifolium/gwsd/logs
-
-# rsync -vuar -P --delete \
-    # santang3@gra-dtn1.computecanada.ca:/home/santang3/scratch/gwsd/slurm_logs/ \
-    # /scratch/projects/trifolium/gwsd/slurm_logs
-
 rsync -vuar -P \
-    santang3@gra-dtn1.computecanada.ca:/home/santang3/scratch/gwsd/results/ \
-    /scratch/projects/trifolium/gwsd/results
+    santang3@gra-dtn1.computecanada.ca:/home/santang3/scratch/gwsd/\
+    /scratch/projects/trifolium/gwsd/
