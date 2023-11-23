5946eaa73319a9f37b1a134a101cecb45dcd0f73
James Santangelo <james.santangelo37@gmail.com>
2023-11-23 17:39:34 -0500

    Increasing ARG region size to 1Mb

--- genomics/workflow/scripts/r/write_arg_regions.R
+++ genomics/workflow/scripts/r/write_arg_regions.R
@@ -3,7 +3,8 @@ library(tidyverse)
 # Load dataframes with windowed sweep statistics
 win_xpnsl_df <- read_delim(snakemake@input[['xpnsl']], delim = '\t')
 
-buffer <- 100000
+set.seed(42)
+buffer <- 425000
 
 urban_rural_windows <- win_xpnsl_df %>% 
     filter(direction %in% c('Urban sel', 'Rural sel'))
@@ -14,7 +15,8 @@ unselected_window <- win_xpnsl_df %>%
 all_regions <- bind_rows(urban_rural_windows, unselected_window) %>% 
     mutate(region = paste0(Chr, ":", start, "-", end),
            regionID = 1:n(),
-           start = start - buffer, end = end + buffer) %>% 
+           start = start - buffer, end = end + buffer) %>%
+    mutate(start = ifelse(start < 0, 0, start)) %>%
     dplyr::select(Chr, start, end, winID, region, regionID, direction, pos_most_diff)
 
 write_delim(x = all_regions, file = snakemake@output[["arg_regions"]], delim = "\t")
